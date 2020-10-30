dacf361c5a2ff7bc94270c9f77371acddae33b59
farahharir <farahben@wanadoo.fr>
2020-10-30 18:31:23 +0100

    mis à jour

--- microservice-compte/src/main/java/com/P7/microservicecompte/dao/CompteUtilisateurDao.java
+++
@@ -1,9 +0,0 @@
-package com.P7.microservicecompte.dao;
-
-import com.P7.microservicecompte.model.CompteUtilisateur;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CompteUtilisateurDao extends JpaRepository<CompteUtilisateur, Long> {
-}
