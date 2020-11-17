9de63825281266b7b1d8803aed415af5361b9a23
farahharir <farahben@wanadoo.fr>
2020-11-17 22:46:12 +0100

    creeation microservice batch

---
+++ microservice-batch/src/main/java/com/P7/microservicebatch/MicroserviceBatchApplication.java
@@ -0,0 +1,13 @@
+package com.P7.microservicebatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceBatchApplication.class, args);
+	}
+
+}
---
+++ microservice-batch/src/test/java/com/P7/microservicebatch/MicroserviceBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.P7.microservicebatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
